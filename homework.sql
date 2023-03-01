42c96a71607de4377c758a83e10f8dcfd4195076
Mark Chigrin <mark@ilmpc.xyz>
2023-03-01 18:28:53 +0400

    Add homework

---
+++ homework.sql
@@ -0,0 +1,10 @@
+/*
+1) Query the names of the students and the courses they are taking
+2) Query the names of all teachers with their competencies
+3) Find a teacher who has no competencies
+4) Find the names of students who do not take any course
+5) Find courses that no student attends
+6) Find competencies that no teacher has
+7) Query the name of the course and the name of the headman
+8) Query the name of the student and the names of the headman that are in the courses he is taking
+*/
--- students.sql
+++ students.sql
@@ -67,3 +67,120 @@
  4 5
  
  */
+-- @block
+CREATE DATABASE uni;
+
+
+USE uni;
+
+
+CREATE TABLE Students (
+  id integer PRIMARY KEY AUTO_INCREMENT,
+  name varchar(128) NOT NULL,
+  age integer
+);
+
+
+-- @block
+CREATE TABLE Teachers (
+  id integer AUTO_INCREMENT,
+  name varchar(128) NOT NULL,
+  age integer,
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE Competencies (
+  id integer AUTO_INCREMENT,
+  title varchar(128) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE TeachersToCompetencies (
+  id integer AUTO_INCREMENT,
+  teacher_id integer,
+  competencies_id integer,
+  FOREIGN KEY (teacher_id) REFERENCES Teachers(id),
+  FOREIGN KEY (competencies_id) REFERENCES Competencies(id),
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE Courses (
+  id integer AUTO_INCREMENT,
+  teacher_id integer,
+  title varchar(128) NOT NULL,
+  headman_id integer,
+  FOREIGN KEY (teacher_id) REFERENCES Teachers(id),
+  FOREIGN KEY (headman_id) REFERENCES Students(id),
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE StudentsToCourses (
+  id integer AUTO_INCREMENT,
+  student_id integer,
+  course_id integer,
+  FOREIGN KEY (student_id) REFERENCES Students(id),
+  FOREIGN KEY (course_id) REFERENCES Courses(id),
+  PRIMARY KEY (id)
+);
+
+
+-- Values
+INSERT INTO Students (name, age)
+VALUES ('Charles R.Campbell', 22),
+  ('Brian L.Forrest', 23),
+  ('Robert K.Lucas', 23),
+  ('Melissa B.Lopez', 51),
+  ('Jessica M.Farrell', 21),
+  ('Inez J.Michel', 36);
+
+
+INSERT INTO Teachers (name, age)
+VALUES ('Lois C.Holmes', 44),
+  ('Evelyn E.Anderson', 38),
+  ('James D.Brown', 56),
+  ('Randall L.Smith', 66),
+  ('Roy S.Barron', 53),
+  ('Kathy G.Howe', 48);
+
+
+INSERT INTO Competencies (title)
+VALUES ('Mathematics'),
+  ('Computer science'),
+  ('Programming'),
+  ('Graphic arts');
+
+
+INSERT INTO TeachersToCompetencies (teacher_id, competencies_id)
+VALUES (1, 1),
+  (2, 1),
+  (2, 3),
+  (3, 2),
+  (4, 1),
+  (5, 3);
+
+
+INSERT INTO Courses (teacher_id, title, headman_id)
+VALUES (1, 'Algebra of logic', 2),
+  (2, 'Mathematical statistics', 3),
+  (4, 'Higher mathematics', 5),
+  (5, 'Javascript', 1),
+  (5, 'Basic Python', 1);
+
+
+INSERT INTO StudentsToCourses (student_id, course_id)
+VALUES (1, 1),
+  (2, 1),
+  (3, 2),
+  (3, 3),
+  (4, 5);
+
+-- @block
+ (1, 'Algebra of logic', 2),
+ (2, 'Mathematical statistics', 3),
+ (4, 'Higher mathematics', 5),
+ (5, 'Javascript', 1),
+ (5, 'Basic Python', 1),
