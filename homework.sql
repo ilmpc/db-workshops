2e3548cd6df36fdb88d39d1253da36896b9488ad
Mark Chigrin <mark@ilmpc.xyz>
2023-03-03 17:34:10 +0400

    Add homework solution

--- homework.sql
+++ homework.sql
@@ -13,7 +13,66 @@ USE uni;
 
 
 -- @block
-SELECT s.name, c.title
+SELECT s.name,
+  c.title
 FROM Students AS s
   INNER JOIN StudentsToCourses AS s2c ON s.id = s2c.student_id
-  INNER JOIN Courses as c on c.id = s2c.course_id;
+  INNER JOIN Courses AS c ON c.id = s2c.course_id;
+
+
+-- @block
+SELECT t.id,
+  t.name,
+  t2c.id c.title
+FROM Teachers AS t
+  LEFT JOIN TeachersToCompetencies AS t2c ON t.id = t2c.teacher_id
+  LEFT JOIN Competencies AS c ON t2c.competencies_id = c.id;
+
+
+-- @block
+SELECT t.id,
+  t.name
+FROM Teachers AS t
+  LEFT JOIN TeachersToCompetencies AS t2c ON t.id = t2c.teacher_id
+WHERE t2c.id IS NULL;
+
+
+-- @block
+SELECT s.name
+FROM Students AS s
+  LEFT JOIN StudentsToCourses AS s2c ON s.id = s2c.student_id
+WHERE s2c.id IS NULL;
+
+
+-- @block
+SELECT c.title
+FROM Courses AS c
+  LEFT JOIN StudentsToCourses AS s2c ON c.id = s2c.course_id
+WHERE s2c.id IS NULL;
+
+
+-- @block
+SELECT c.title
+FROM Competencies AS c
+  LEFT JOIN TeachersToCompetencies AS t2c ON c.id = t2c.competencies_id
+WHERE t2c.id IS NULL;
+
+
+-- @block
+-- Query the name of the course and the name of the headman
+SELECT c.title,
+  s.name
+FROM Courses AS c
+  INNER JOIN Students AS s ON c.headman_id = s.id;
+
+
+-- @block
+-- Query the name of the student and the names of the headman that are in the courses he is taking
+SELECT s.id,
+  s.name,
+  c.headman_id,
+  hds.name AS hds_name
+FROM Students AS s
+  INNER JOIN StudentsToCourses AS s2c ON s.id = s2c.student_id
+  INNER JOIN Courses AS c ON c.id = s2c.course_id
+  INNER JOIN Students AS hds ON c.headman_id = hds.id;
